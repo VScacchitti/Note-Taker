f4dba60131f29f33ba7edf03c7131e4991a3a9e6
Vince Scacchitti <VScacchitti24@gmail.com>
2020-10-15 12:04:08 -0400

    added get request for db.json, and post request that creates a new note, and writes it to the db.json.

--- server.js
+++ server.js
@@ -12,8 +12,51 @@ const PORT = process.env.PORT || 3500;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
-//API Routes
 
+//________API Routes______________________
+
+//global array that reads JSON and returns our db.json
+let globalSavedNotes = util.promisify(fs.readFile);
+function retrieveSavedNotes() {
+  console.log("Saved Notes", globalSavedNotes("./db/db.json", "utf8"));
+  return (savedNotes = globalSavedNotes("./db/db.json", "utf8"));
+}
+
+//get route for the db.json file
+app.get("/api/notes", (req, res) => {
+  retrieveSavedNotes()
+    .then((savedNotes) => {
+      // sends to the front end
+      res.send(JSON.parse(savedNotes));
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+//post request that recieves new note, and adds it db.json file
+app.post("/api/notes", (req, res) => {
+  // reads db.json file
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  //generates a random ID for each note
+  let id = crypto.randomBytes(16).toString("hex");
+  let newNote = {
+    title: req.boby.title,
+    text: req.body.text,
+    id: id,
+  };
+
+  console.log("newNote", newNote);
+
+  savedNotes.push(newNote);
+
+  //write new note to db.json
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes), (err) => {
+    if (err) throw err;
+    console.log("error");
+  });
+
+  console.log("A new note has been created!");
+  return res.json(savedNotes);
+});
 //HTML routes
 
 //Start server listening
