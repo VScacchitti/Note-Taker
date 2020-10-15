cac5b75467536ffba9e176a421ca6719c489de80
Vince Scacchitti <VScacchitti24@gmail.com>
2020-10-15 10:38:47 -0400

    Created dependencies.

---
+++ server.js
@@ -0,0 +1,13 @@
+//Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const crypto = require("crypto");
+
+//Set up Express
+
+//MiddleWare
+
+//API Routes
+
+//HTML routes
