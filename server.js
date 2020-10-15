c1afbb830f0640e9458cd8e2e8d1b19602818970
Vince Scacchitti <VScacchitti24@gmail.com>
2020-10-15 10:50:49 -0400

    added one more dependency, and added middleware to handle data parsing, and file readability.

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
 
 //Set up Express
 const app = express();
 const PORT = process.env.PORT || 3500;
-//MiddleWare
-
+//MiddleWare - handles parsing data and file readability
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 //API Routes
 
 //HTML routes
