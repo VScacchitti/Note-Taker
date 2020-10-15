b180cfeb1d3eda8aec7c2d5205932ceabce606fe
Vince Scacchitti <VScacchitti24@gmail.com>
2020-10-15 10:44:21 -0400

    created express app and is listening on desired port.

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const fs = require("fs");
 const crypto = require("crypto");
 
 //Set up Express
-
+const app = express();
+const PORT = process.env.PORT || 3500;
 //MiddleWare
 
 //API Routes
 
 //HTML routes
+
+//Start server listening
+app.listen(PORT, () => {
+  console.log("App is listening on PORT:" + PORT);
+});
